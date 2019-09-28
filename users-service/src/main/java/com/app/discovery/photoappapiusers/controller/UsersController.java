f2573150a6e12930dea6cd7b8f9e59e428ed4f9f
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-09-28 11:58:40 +0300

    added createUserRequestModel; added validation on it.

--- users-service/src/main/java/com/app/discovery/photoappapiusers/controller/UsersController.java
+++ users-service/src/main/java/com/app/discovery/photoappapiusers/controller/UsersController.java
@@ -1,11 +1,12 @@
 package com.app.discovery.photoappapiusers.controller;
 
 
+import com.app.discovery.photoappapiusers.ui.model.CreateUserRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -17,5 +18,11 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status() {
         return "Working on port: " + env.getProperty("local.server.port");
+    }
+
+
+    @PostMapping
+    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+        return "dfsfs as fas create User...";
     }
 }
