97f00f7b933291173d71a4a5a1982de5db878784
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-10-06 08:16:55 +0300

    added spring bus support(updating properties from config server) via rabbitMQ

--- users-service/src/main/java/com/app/discovery/photoappapiusers/controller/UsersController.java
+++ users-service/src/main/java/com/app/discovery/photoappapiusers/controller/UsersController.java
@@ -28,7 +28,7 @@ public class UsersController {
 
     @GetMapping("/status/check")
     public String status() {
-        return "Working on port: " + env.getProperty("local.server.port");
+        return "Working on port: " + env.getProperty("local.server.port") + " with token: " + env.getProperty("token.secret");
     }
 
 
