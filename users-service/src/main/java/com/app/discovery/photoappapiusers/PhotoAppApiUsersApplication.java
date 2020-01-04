4471ca6f2ae06149124cd378861aeef4aa6ccd1b
Alex Zosyk <alekcandrzosyk@gmail.com>
2020-01-04 22:29:56 +0200

    added circuit breaker hystrix

--- users-service/src/main/java/com/app/discovery/photoappapiusers/PhotoAppApiUsersApplication.java
+++ users-service/src/main/java/com/app/discovery/photoappapiusers/PhotoAppApiUsersApplication.java
@@ -3,6 +3,7 @@ package com.app.discovery.photoappapiusers;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
     public static void main(String[] args) {
--- users-service/src/main/java/com/app/discovery/photoappapiusers/data/AlbumsServiceClient.java
+++ users-service/src/main/java/com/app/discovery/photoappapiusers/data/AlbumsServiceClient.java
@@ -2,14 +2,24 @@ package com.app.discovery.photoappapiusers.data;
 
 import com.app.discovery.photoappapiusers.ui.model.AlbumResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient("albums-ws")
+@FeignClient(value = "albums-ws", fallback = AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        return new ArrayList<>();
+    }
 }
