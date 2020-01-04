35bd2c1af0bec9efd7af1f781c68d2bb5dafc7a4
Alex Zosyk <alekcandrzosyk@gmail.com>
2020-01-04 12:41:39 +0200

    added FeignErrorDecoder

---
+++ users-service/src/main/java/com/app/discovery/photoappapiusers/shared/FeignErrorDecoder.java
@@ -0,0 +1,26 @@
+package com.app.discovery.photoappapiusers.shared;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class FeignErrorDecoder implements ErrorDecoder {
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        switch (response.status()) {
+            case 400:
+                break;
+            case 404:
+                if(methodKey.contains("getAlbums")) {
+                    return new ResponseStatusException(HttpStatus.valueOf(response.status()), "Users albums are not found.");
+                }
+                break;
+            default:
+                return new Exception(response.reason());
+        }
+        return null;
+    }
+}
