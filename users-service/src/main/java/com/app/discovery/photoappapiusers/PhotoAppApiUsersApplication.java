b9a3d2abc16e68c3117e9f2c87d2ae234be7e590
Alex Zosyk <alekcandrzosyk@gmail.com>
2020-01-04 12:12:45 +0200

    switched on debug logging to feign client

--- users-service/src/main/java/com/app/discovery/photoappapiusers/PhotoAppApiUsersApplication.java
+++ users-service/src/main/java/com/app/discovery/photoappapiusers/PhotoAppApiUsersApplication.java
@@ -1,5 +1,6 @@
 package com.app.discovery.photoappapiusers;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -19,5 +20,10 @@ public class PhotoAppApiUsersApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
