8b9ae751c49ca710e224304e8328e521dac4cfc0
Alex Zosyk <alekcandrzosyk@gmail.com>
2020-01-11 13:25:58 +0200

    added feign callback factory

--- users-service/src/main/java/com/app/discovery/photoappapiusers/data/AlbumsServiceClient.java
+++ users-service/src/main/java/com/app/discovery/photoappapiusers/data/AlbumsServiceClient.java
@@ -1,6 +1,7 @@
 package com.app.discovery.photoappapiusers.data;
 
 import com.app.discovery.photoappapiusers.ui.model.AlbumResponseModel;
+import feign.hystrix.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(value = "albums-ws", fallback = AlbumsFallback.class)
+@FeignClient(value = "albums-ws", fallbackFactory = AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
 }
 
+
 @Component
-class AlbumsFallback implements AlbumsServiceClient {
+class AlbumsFallback implements FallbackFactory<AlbumsServiceClient> {
+
     @Override
-    public List<AlbumResponseModel> getAlbums(String id) {
-        return new ArrayList<>();
+    public AlbumsServiceClient create(Throwable throwable) {
+        return id -> new ArrayList<>();
     }
 }
